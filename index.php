b79dbc759975f1c50df8663db8d71d6db8b61de8
SwissPvP2003 <noahziltener@yahoo.com>
2018-11-29 08:17:13 +0100

    Automatischer Zeilen-Umbruch

--- index.php
+++ index.php
@@ -12,6 +12,19 @@
         <h1 class="form-title">Noahs Blog</h1>
         <p>Das ist der persöndliche Blog von mir.</p>
 <?php
+    function insertSpace($text, $pos) { 
+        $new = $text;
+        for ($i = 0; $i < strlen($text); $i += $pos) {
+
+            $vor = substr($new, 0, $i);
+            $nach = substr($new, $i);
+
+            if (strpos($vor, " ")  == false) {
+                    $new = $vor . ' ' . $nach; 
+            }
+        } 
+        return $new;
+    }
     $user = 'root';
     $pass = '';
     $dbh = new PDO('mysql:host=localhost;dbname=blogdb', $user, $pass);
@@ -23,7 +36,7 @@
         <div class="form-actions">
             <h1><?= htmlspecialchars($output['created_by'], ENT_QUOTES, "UTF-8"); ?></h1>
             <h2><?= htmlspecialchars($output['post_title'], ENT_QUOTES, "UTF-8"); ?></h2>
-            <p><?= htmlspecialchars($output['post_text'], ENT_QUOTES, "UTF-8"); ?></p>
+            <p><?= insertSpace(htmlspecialchars($output['post_text'], ENT_QUOTES, "UTF-8"), 60); ?></p>
             <?php if( htmlspecialchars($output['post_image'], ENT_QUOTES, "UTF-8") !== ''){
             ?><img class="post_images" src="<?= htmlspecialchars($output['post_image'], ENT_QUOTES, "UTF-8"); ?>" onError="this.src='ersatzbild.png';" alt="Bild nicht verfügbar" /><?php
         }
